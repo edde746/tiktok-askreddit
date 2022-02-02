808e4f1061ca57984593a6248c4c2bf33eb8d0b1
edde746 <86283021+edde746@users.noreply.github.com>
2022-02-02 18:51:01 +0100

    Update edge-tts import

--- tts.py
+++ tts.py
@@ -1,12 +1,12 @@
 import random
-import edgeTTS
+import edge_tts
 
 async def get_voice():
-    voices = [voice for voice in await edgeTTS.list_voices() if "en-" in voice["Locale"]]
+    voices = [voice for voice in await edge_tts.list_voices() if "en-" in voice["Locale"]]
     return random.choice(voices)["Name"]
 
 async def generate(text,name,voice):
-    communicate = edgeTTS.Communicate()
+    communicate = edge_tts.Communicate()
     with open(f"output/{name}.mp3","wb") as fp:
         async for i in communicate.run(text,voice=voice):
             if i[2] is not None:
